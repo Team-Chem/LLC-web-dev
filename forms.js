8810da6ed08719c41f8975d52e21305396ee61ed
My Info Dixon <nathanielwdixon07@gmail.com>
2022-09-20 23:53:27 -0500

    Improved Forms and Results

--- forms.js
+++ forms.js
@@ -11,6 +11,9 @@
 var submitButton = document.querySelector(".submitbutton");
 var submitButton2 = document.querySelector(".submitbutton2");
 var submitButton3 = document.querySelector(".submitbutton3");
+var submitButton4 = document.querySelector(".submitbutton4");
+var submitButton5 = document.querySelector(".submitbutton5");
+var submitButton6 = document.querySelector(".submitbutton6");
 var options = document.querySelector(".form-options");
 var btn1 = document.getElementById("btn1");
 var form1Sub = document.querySelector("#polymer-data-form");
@@ -19,8 +22,17 @@ var form2Sub = document.querySelector("#Mobile-phase-form");
 var form1;
 var form2;
 var form3;
+var form4;
+var form5;
+var form6;
 var btn3 = document.getElementById("btn3");
 var form3Sub = document.querySelector("#stationary-phase-form");
+var btn4 = document.getElementById("btn4");
+var form4Sub = document.querySelector("#condition-form");
+var btn5 = document.getElementById("btn5");
+var form5Sub = document.querySelector("#investigation-form");
+var btn6 = document.getElementById("btn6");
+var form6Sub = document.querySelector("#upload-form");
 //Did not need this just set base display to none */
 /*document.addEventListener("load", () => {
     form = document.getElementById("polymer-data-form");
@@ -47,6 +59,25 @@ function Submit3(){
     }
 }
 
+function Submit4(){
+    if(form4Sub.checkValidity() === true){
+        submitButton4.className = "submitbutton4 show";
+    }
+}
+
+function Submit5(){
+    if(form5Sub.checkValidity() === true){
+        submitButton5.className = "submitbutton5 show";
+    }
+}
+
+function Submit6(){
+    if(form6Sub.checkValidity() === true){
+        submitButton6.className = "submitbutton6 show";
+    }
+}
+
+//These are functions made to reset the previous forms to display none when user clicks on a new form.
 function DefaultForm1(){
     form1 = document.getElementById("polymer-data-form");
     form1.style.display = "none";
@@ -62,6 +93,21 @@ function DefaultForm3(){
     form3.style.display = "none";
 }
 
+function DefaultForm4(){
+    form4 = document.getElementById("condition-form");
+    form4.style.display = "none";
+}
+
+function DefaultForm5(){
+    form5 = document.getElementById("investigation-form");
+    form5.style.display = "none";
+}
+
+function DefaultForm6(){
+    form6 = document.getElementById("upload-form");
+    form6.style.display = "none";
+}
+
 //Each of these are meant to display a particular field based on the user clicking.
 btn1.addEventListener("click", () => {
     form1 = document.getElementById("polymer-data-form");
@@ -70,6 +116,9 @@ btn1.addEventListener("click", () => {
         form1.style.display = "block";
         DefaultForm2();
         DefaultForm3();
+        DefaultForm4();
+        DefaultForm5();
+        DefaultForm6();
     }
     else {
         form1.style.display = "none";
@@ -84,6 +133,9 @@ btn2.addEventListener("click", () =>  {
         form2.style.display = "block";
         DefaultForm1();
         DefaultForm3();
+        DefaultForm4();
+        DefaultForm5();
+        DefaultForm6();
     }
     else {
         form2.style.display = "none";
@@ -97,15 +149,72 @@ btn3.addEventListener("click", () => {
         form3.style.display = "block";
         DefaultForm1();
         DefaultForm2();
+        DefaultForm4();
+        DefaultForm5();
+        DefaultForm6();
     }
     else {
         form3.style.display = "none";
     }
 });
 
-//These are to enabel the submit button option after valid inputs.
+btn4.addEventListener("click", () => {
+    form4 = document.getElementById("condition-form");
+
+    if(form4.style.display === "none"){
+        form4.style.display = "block";
+        DefaultForm1();
+        DefaultForm2();
+        DefaultForm3();
+        DefaultForm5();
+        DefaultForm6();
+    }
+    else {
+        form4.style.display = "none";
+    }
+});
+
+btn5.addEventListener("click", () => {
+    form5 = document.getElementById("investigation-form");
+
+    if(form5.style.display === "none"){
+        form5.style.display = "block";
+        DefaultForm1();
+        DefaultForm2();
+        DefaultForm3();
+        DefaultForm4();
+        DefaultForm6()
+    }
+    else {
+        form5.style.display = "none";
+    }
+});
+
+btn6.addEventListener("click", () => {
+    form6 = document.getElementById("upload-form");
+
+    if(form6.style.display === "none"){
+        form6.style.display = "block";
+        DefaultForm1();
+        DefaultForm2();
+        DefaultForm3();
+        DefaultForm4();
+        DefaultForm5();
+    }
+    else {
+        form6.style.display = "none";
+    }
+});
+
+//These are to enable the submit button option after valid inputs.
 form1Sub.addEventListener("change", Submit1, false);
 
 form2Sub.addEventListener("change", Submit2, false);
 
 form3Sub.addEventListener("change", Submit3, false);
+
+form4Sub.addEventListener("change", Submit4, false);
+
+form5Sub.addEventListener("change", Submit5, false);
+
+form6Sub.addEventListener("change", Submit6, false);
