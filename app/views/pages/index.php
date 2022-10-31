017e0557a922c4e24af80db8b0c70385de371fd1
Matt Hosier <48217372+mrhosier42@users.noreply.github.com>
2022-10-30 20:08:54 -0500

    organized profile page a bit and fixed the default character color. Cleaned up some whitespace

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,4 +1,5 @@
 <?php
+    $title = 'About';
     include "header.php";
 ?>
         <header>
--- app/views/pages/process-signup.php
+++ app/views/pages/process-signup.php
@@ -1,5 +1,4 @@
 <?php
-
 include_once "../../../db/connection.php";
 
 // If name="FirstName" is empty then it will print the message and then exit
