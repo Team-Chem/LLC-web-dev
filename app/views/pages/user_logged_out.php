4e9e1e659e1c8434192ee59c3d4cc03e45a1836f
Hunter <98065056+TrashDeviance@users.noreply.github.com>
2022-12-05 17:57:59 -0600

    Update user_logged_out.php

--- app/views/pages/user_logged_out.php
+++ app/views/pages/user_logged_out.php
@@ -1,8 +1,10 @@
 <?php
 
+// Destroys the session for the user when they click sign out button on the navbar.
 session_start();
 
 session_destroy();
 
+// Redirect the user back to the sign in page.
 header("Location: sign_in.php");
 exit();
