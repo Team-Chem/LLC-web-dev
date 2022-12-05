a32c2275eaee1c54cde32d8c599d0cfc2689a3be
Hunter <98065056+TrashDeviance@users.noreply.github.com>
2022-12-05 17:58:46 -0600

    Update email_validator.php

--- app/views/pages/email_validator.php
+++ app/views/pages/email_validator.php
@@ -3,14 +3,17 @@
 
 $conn = require __DIR__ . "/../../../db/connection.php";
 
+/* Selects the email attribute from the database */
 $sql = sprintf("SELECT * FROM user WHERE email = '%s'", $conn->real_escape_string($_GET['email']));
 
 $result = $conn->query($sql);
 
+// Stores the $results value into another variable
 $available_email = $result->num_rows === 0;
 
 header("Content-Type: application/json");
 
+// Echos back the value to determine if the email is available or not
 echo json_encode(["available" => $available_email]);
 
 ?>
