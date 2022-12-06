5b444ed5b6f32bbfaa57c151a1f18a8cc91d5491
Hunter <98065056+TrashDeviance@users.noreply.github.com>
2022-12-05 18:00:53 -0600

    Update form_validation.js

--- app/views/pages/form_validation.js
+++ app/views/pages/form_validation.js
@@ -1,4 +1,10 @@
+//Using JustValidate to implement registration validation for the following fields
 const validation = new JustValidate('#sign-up-form');
+
+/* Adding validation for the first_name_sign_up and last_name_sign_up ids. Making it a required field, minimum length of 3, 
+   maximum length of 30, and adding custom regex to eliminate special characters or numbers from being entered.
+   Also, making the first letter must be capitalized.
+*/
 
 validation
     .addField('#first_name_sign_up', [
@@ -29,6 +35,10 @@ validation
             errorMessage: 'First letter must be capitalized, no numbers, or special characters',
         },
     ])
+    
+    /* Validation for email. Grabbing the email_validator.php file and and determining if the email enterd is considered as a legitimate
+    email or not. This also checks to see if the email is has already been used in a previous registration. */
+    
     .addField('#email_sign_up', [
         {
             rule: 'required'
@@ -50,6 +60,9 @@ validation
             errorMessage: 'Enter a valid email address!',
         },
     ])
+    
+    /* Validation for email. Making it required and both the password and the confirm password must be matching. */
+    
     .addField('#password_sign_up', [
         {
             rule: 'required'
